639ea66e9ab2949287782b3977f5a08a03f58177
gabriel <gabrielmontsilva16@gmail.com>
2026-06-08 17:28:16 -0300

    reformular services e criar logica portal

--- src/app/pages/portal/portal-component/portal-component.ts
+++ src/app/pages/portal/portal-component/portal-component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../../../services/news';
+import { News } from '../../../models/news';
 @Component({
   selector: 'app-portal-component',
   standalone: false,
   templateUrl: './portal-component.html',
-  styleUrl: './portal-component.css',
+  styleUrl: './portal-component.css'
 })
-export class PortalComponent {}
+export class PortalComponent implements OnInit {
+
+  newsList: News[] = [];
+
+  constructor(private newsService: NewsService) {}
+
+  ngOnInit(): void {
+    this.newsService.getAll().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.newsList = data;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+}
