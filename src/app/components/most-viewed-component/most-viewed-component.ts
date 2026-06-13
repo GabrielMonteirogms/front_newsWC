f003bd5b7a8e916bb0b8a6eced28925a3a2cf90e
GabrielMonteirogms <gabrielmontsilva16@gmail.com>
2026-06-13 20:14:29 -0300

    finalização aba das noticias mais vistas no portal

--- src/app/components/most-viewed-component/most-viewed-component.ts
+++ src/app/components/most-viewed-component/most-viewed-component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../../services/news';
+import { News } from '../../models/news';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-most-viewed-component',
   standalone: false,
   templateUrl: './most-viewed-component.html',
-  styleUrl: './most-viewed-component.css',
+  styleUrls: ['./most-viewed-component.css']
 })
-export class MostViewedComponent {}
+export class MostViewedComponent implements OnInit {
+
+  mostViewedNews: News[] = [];
+
+  constructor(
+    private newsService: NewsService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+
+    console.log('MOST VIEWED CARREGOU');
+
+    this.newsService.getMostViewed().subscribe({
+
+      next: (data) => {
+
+        this.mostViewedNews = data;
+
+        this.cdr.detectChanges();
+
+      },
+
+      error: (err) => {
+
+        console.error(err);
+
+      }
+
+    });
+
+  }
+
+}
